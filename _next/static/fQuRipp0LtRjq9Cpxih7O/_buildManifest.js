aee3f8863b0c290d14cb0a5130af61769c64ba10
the-red <the-red@users.noreply.github.com>
2021-03-06 17:18:56 +0000

    deploy: be379dad1bcda739964d0f29d6565e8d08cb3cdf

---
+++ _next/static/fQuRipp0LtRjq9Cpxih7O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F2a760f1ea6b9234d869a.css","static\u002Fchunks\u002Fpages\u002Findex-091dc94f2f38aa48d810.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6a78c96636e3e8c7f5be.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
